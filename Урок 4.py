fd56bb10c6b2a4c6cd79e1a213bd16e81066b0a5
Ilimloo <101550435+Ilimloo@users.noreply.github.com>
2023-01-19 22:24:15 +0600

    Add files via upload

---
+++ Урок 4.py
@@ -0,0 +1,14 @@
+#Задача 1
+# number = int(input("Введи число: "))
+# print(number, "*", number, "=", number * number)
+
+#Задача 2
+# num1 = int(input("Введи число1: "))
+# num2 = int(input("Введи число2: "))
+# print(num1, "/", num2, "=", num1 / num2)
+
+#Задача 3
+name = input("Как тебя зовут? ")
+age = int(input("Сколько тебе лет? "))
+print("Привет,", name)
+print("Через", 100 - age, "лет/год/года тебе будет 100 лет")
