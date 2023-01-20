cff0a17d93d428c74532db9a3f0e0ac4ae2b8673
Ilimloo <101550435+Ilimloo@users.noreply.github.com>
2023-01-20 13:26:48 +0600

    Add files via upload

---
+++ Урок 16.py
@@ -0,0 +1,24 @@
+## Problem 1
+# count = 0 #Переменная счетчик
+# for i in range(100):
+#     count = count + i
+# print("Сумма всех чисел от 0 до 100 =", count)
+
+## Problem 2
+# product = 1
+# for i in range(1, 10):
+#     product = product * i
+# print(product)
+
+##Problem 3
+# sum = 0
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         sum = sum + i
+# print(sum)
+
+## Problem 4
+n = int(input())
+for i in range(n):
+    if i % 2 == 1:
+        print(i)
